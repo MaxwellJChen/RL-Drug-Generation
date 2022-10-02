95f7e983663fb7487c10543e2c531627bdee9038
DollarDebt <dollardebt5@gmail.com>
2022-10-02 13:26:10 -0400

    Test

--- preprocessing/ChEMBLdb_31.py
+++ preprocessing/ChEMBLdb_31.py
@@ -4,4 +4,4 @@ import rdkit
 
 # Create list of SMILES from ChEMBL data
 filepath = "/Users/maxwellchen/Desktop/Drug_Design/Data/ChEMBL_31/chembl_31_chemreps.txt"
-pandas = pandas.read_csv(filepath, sep = '/t')
+df = pandas.read_csv(filepath, sep = '/t')
