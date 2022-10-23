7909a1c6e31e7cf9daf3361d91a00324b0edf51b
DollarDebt <dollardebt5@gmail.com>
2022-10-23 01:17:29 -0400

    Test

---
+++ preprocessing/test.py
@@ -0,0 +1,7 @@
+import numpy as np
+asdf = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(asdf.index(11))
+
+for i in range(1, 4):
+    arr = np.load(f'/Users/maxwellchen/PycharmProjects/Drug_Design/RL-Drug-Generation/preprocessing/charges{i}.npy')
